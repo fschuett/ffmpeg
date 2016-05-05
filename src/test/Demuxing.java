9a88a4b78af5ac3274c687d638744a2584e323f0
fschuett <fschuett@gymnasium-himmelsthuer.de>
2016-05-05 11:59:35 +0200

    fix alloc_frame -> av__frame_alloc

--- src/test/Demuxing.java
+++ src/test/Demuxing.java
@@ -277,7 +277,7 @@ public class Demuxing {
 				System.exit(1);
 			}
 
-			// FIXME: frame = alloc_frame(); 3.0.2
+			frame = AvutilLibrary.av__frame_alloc();
 			if (frame == null) {
 				System.err.printf("Could not allocate frame\n");
 				System.exit(1);
